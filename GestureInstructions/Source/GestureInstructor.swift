2fb8303f0319e50ae88c13b725300b98dfc4c859
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-11-18 16:14:22 +0500

    add enums

---
+++ GestureInstructions/Source/GestureInstructor.swift
@@ -0,0 +1,24 @@
+//
+//  GestureAssistant.swift
+//  GestureInstructions
+//
+//  Created by Rinat Enikeev on 11/18/19.
+//  Copyright © 2019 Rinat Enikeev. All rights reserved.
+//
+
+import Foundation
+
+public enum GISwipeDirection {
+    case up
+    case down
+    case left
+    case right
+}
+
+public enum GITap {
+    case single
+    case longPress
+    case double
+}
+
+
