ccbb875729d01b11803e3f74e4819c97c07ffd9c
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-11-19 15:56:44 +0500

    use image

--- Example/GestureInstructions/ViewController.swift
+++ Example/GestureInstructions/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        GestureInstructor.appearance.tapImage = UIImage(named: "hand")
     }
 
     @IBAction func showButtonTouchUpInside(_ sender: Any) {
